6b088519f2b4c1e946354588f0a7313dd595fb2b
Benjamin Meyer <bmeyer@benjamindmeyer.com>
2015-12-18 12:13:18 -0500

    server "z"s

--- scapyserver.py
+++ scapyserver.py
@@ -5,6 +5,15 @@ characterlist = list(text.upper())
 characterstring = ""
 index = 0
 
+character1 = hex(ord("Z"))[2:]+hex(ord("Z"))[2:]
+characterstring = '0x'+character1
+suffixnum = int(characterstring,16)
+print suffixnum
+index += 1
+a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = suffixnum)
+send(a)
+
+
 while (index < len(characterlist)):
 	character1 = hex(ord("Z"))[2:]+hex(ord(characterlist[index]))[2:]
 	characterstring = '0x'+character1
@@ -14,3 +23,11 @@ while (index < len(characterlist)):
 	a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = suffixnum)
 	send(a)
 
+character1 = hex(ord("Z"))[2:]+hex(ord("Z"))[2:]
+characterstring = '0x'+character1
+suffixnum = int(characterstring,16)
+print suffixnum
+index += 1
+a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = suffixnum)
+send(a)
+send(a)
