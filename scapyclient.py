aaa983edea519f92fbf2a7c80b4786ec53aff030
Benjamin Meyer <bmeyer@benjamindmeyer.com>
2015-12-18 09:20:42 -0800

    message begin and end

--- scapyclient.py
+++ scapyclient.py
@@ -7,10 +7,16 @@ def customAction(packet):
 	packetCount += 1
 	print "Packet #" + str(packetCount) + ": " +packet[0][1].src + "==>" + packet[0][1].dst + ": " + str(packet[0].SUFFIX)
 	suffixnum = packet[0].SUFFIX
+	if(suffixnum == 23130):
+		message = True
+	if(suffixnum == 23130):
+		messageend1 = True
+	if(messageend1 and suffixnum == 23130):
+		message = False
 	asciivalue = hex(suffixnum)[4:]
 	asciivalue = int(asciivalue, 16)
 	character = chr(asciivalue)
-	return "! " + character + " !"
-
+		if(message):
+			return "! " + character + " !"
 
 sniff(filter="port 137", prn=customAction)
