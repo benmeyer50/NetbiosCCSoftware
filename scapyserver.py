781f7702a7de1ad8bf0846e72d0c988bf531556c
Benjamin Meyer <bmeyer@benjamindmeyer.com>
2015-12-18 14:18:41 -0800

    prefix

--- scapyserver.py
+++ scapyserver.py
@@ -5,12 +5,7 @@ characterlist = list(text.upper())
 characterstring = ""
 index = 0
 
-character1 = hex(ord("Z"))[2:]+hex(ord("Z"))[2:]
-characterstring = '0x'+character1
-suffixnum = int(characterstring,16)
-print suffixnum
-index += 1
-a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = suffixnum)
+a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = 23130)
 send(a)
 
 
