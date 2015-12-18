54351789d3508dc5286a3245138a0d8e97a84ffd
Benjamin Meyer <bmeyer@benjamindmeyer.com>
2015-12-18 12:07:03 -0500

    updates

--- scapyclient.py
+++ scapyclient.py
@@ -7,7 +7,7 @@ def customAction(packet):
 	packetCount += 1
 	print "Packet #" + str(packetCount) + ": " +packet[0][1].src + "==>" + packet[0][1].dst + ": " + str(packet[0].SUFFIX)
 	suffixnum = packet[0].SUFFIX
-	asciivalue = suffixnum - 16705
+	asciivalue = suffixnum-90
 	character = chr(asciivalue)
 	return "! " + character + " !"
 
--- scapyserver.py
+++ scapyserver.py
@@ -6,10 +6,10 @@ characterstring = ""
 index = 0
 
 while (index < len(characterlist)):
-	character1 = hex(ord("Z"))[2:]+hex(ord(characterlist[index+1]))[2:]
+	character1 = hex(ord("Z"))[2:]+hex(ord(characterlist[index]))[2:]
 	characterstring = '0x'+character1
 	suffixnum = int(characterstring,16)
-	print characterstring
+	print suffixnum
 	index += 1
 	a = IP(src = '1.1.1.1', dst = '192.168.235.255')/UDP(sport = 8, dport = 137)/NBNSQueryRequest(SUFFIX = suffixnum)
 	send(a)
